2fdf1feddbbc307d682fdae9a187aa5ed9f5ce88
“VenuReddy773” <venu706@gmail.com>
2021-12-06 22:43:09 +0530

    [VenuGopalReddy] Added: Adding Persons into List

---
+++ LamdaDemo/LamdaDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LamdaDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> listPersonsInCity = new List<Person>();
+            AddRecords(listPersonsInCity);           
+        }
+        private static void AddRecords(List<Person> listPersonsInCity)
+        {
+            listPersonsInCity.Add(new Person("111111", "venu", "hyd1", 15));
+            listPersonsInCity.Add(new Person("222222", "Gopal", "hyd2", 65));
+            listPersonsInCity.Add(new Person("333333", "Reddy", "hyd3", 35));
+            listPersonsInCity.Add(new Person("444444", "Attla", "hyd4", 17));            
+        }
+    }
+}
+
